4046cdb013189747e5eac6fa0e89270976bd0ce7
Juan Zurita <Juan.Zurita@fundacion-jala.org>
2021-05-03 11:01:31 -0400

    Prueba con 4620, resultado = [2,3,5,7,11]

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
         NumerosPrimos numerosPrimos = new NumerosPrimos(); 
-        ArrayList<Integer> numPrimos = numerosPrimos.generate(9);
+        ArrayList<Integer> numPrimos = numerosPrimos.generate(4620);
         JOptionPane.showMessageDialog(null, "Los números primos son: " + numPrimos.toString(), "RESULTADO", JOptionPane.INFORMATION_MESSAGE);
     }
 }
