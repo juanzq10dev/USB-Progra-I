a622531f28df0464a7d357505e99648bdca1cc57
Juan Zurita <Juan.Zurita@fundacion-jala.org>
2021-05-03 08:56:39 -0400

    Prueba con 6, resultado: [2,3]

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         NumerosPrimos numerosPrimos = new NumerosPrimos(); 
-        ArrayList<Integer> numPrimos = numerosPrimos.generate(4);
+        ArrayList<Integer> numPrimos = numerosPrimos.generate(6);
         System.out.println(numPrimos.toString());
 
     }
