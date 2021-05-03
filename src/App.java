3dd5f212ef6b50c45aefc14dd98cc7618a437d91
Juan Zurita <Juan.Zurita@fundacion-jala.org>
2021-05-03 08:36:33 -0400

    Prueba con el valor 1: resultado = un array vacio

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         NumerosPrimos numerosPrimos = new NumerosPrimos(); 
-        ArrayList<Integer> numPrimos = numerosPrimos.generate(2);
+        ArrayList<Integer> numPrimos = numerosPrimos.generate(1);
         for(int i : numPrimos){
             System.out.println(i);
         }
