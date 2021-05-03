1820bd89487592756ce732e331ead5ae092517ea
Juan Zurita <Juan.Zurita@fundacion-jala.org>
2021-05-03 10:59:57 -0400

    Prueba con 9, resultado = [3]

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
         NumerosPrimos numerosPrimos = new NumerosPrimos(); 
-        ArrayList<Integer> numPrimos = numerosPrimos.generate(8);
+        ArrayList<Integer> numPrimos = numerosPrimos.generate(9);
         JOptionPane.showMessageDialog(null, "Los números primos son: " + numPrimos.toString(), "RESULTADO", JOptionPane.INFORMATION_MESSAGE);
     }
 }
