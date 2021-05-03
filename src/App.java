900a6b9cca6168e9516864e5e74b3988aea5c838
Juan Zurita <Juan.Zurita@fundacion-jala.org>
2021-05-03 11:07:05 -0400

    Se agrego un JOptionPane para poder escoger cualquier numero

--- src/App.java
+++ src/App.java
@@ -5,8 +5,9 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        NumerosPrimos numerosPrimos = new NumerosPrimos(); 
-        ArrayList<Integer> numPrimos = numerosPrimos.generate(4620);
+        NumerosPrimos numerosPrimos = new NumerosPrimos();
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número"));
+        ArrayList<Integer> numPrimos = numerosPrimos.generate(numero);
         JOptionPane.showMessageDialog(null, "Los números primos son: " + numPrimos.toString(), "RESULTADO", JOptionPane.INFORMATION_MESSAGE);
     }
 }
