354cf27f2b72ff5240898ac5ad7616d185a19ac2
Juan Zurita <Juan.Zurita@fundacion-jala.org>
2021-05-03 08:39:06 -0400

    Prueba con el 2, resultado = [2]; se cambio el for en un println(numPrimos.toString()

--- src/App.java
+++ src/App.java
@@ -3,10 +3,8 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         NumerosPrimos numerosPrimos = new NumerosPrimos(); 
-        ArrayList<Integer> numPrimos = numerosPrimos.generate(1);
-        for(int i : numPrimos){
-            System.out.println(i);
-        }
+        ArrayList<Integer> numPrimos = numerosPrimos.generate(2);
+        System.out.println(numPrimos.toString());
 
     }
 }
