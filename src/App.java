c66bb449108abe825ca6b043aa3df0eaeda43294
Juan Zurita <Juan.Zurita@fundacion-jala.org>
2021-05-03 08:28:27 -0400

    Primera prueba numeros primos, aun no funciona correctamente

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        NumerosPrimos numerosPrimos = new NumerosPrimos(); 
+        ArrayList<Integer> numPrimos = numerosPrimos.generate(2);
+        for(int i : numPrimos){
+            System.out.println(i);
+        }
+
+    }
+}
---
+++ src/NumerosPrimos.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+public class NumerosPrimos {
+    public ArrayList<Integer> generate(int numeroDescomponer) {
+        ArrayList<Integer> numerosPrimos = new ArrayList<Integer>();
+        int divisor = 2;
+        while (divisor <= numeroDescomponer) {
+            int producto = numeroDescomponer % divisor;
+            if (numerosPrimos.size() > 0) {
+                for (int i : numerosPrimos) {
+                    producto = divisor % i;
+                    if (producto == 0) {
+                        break;
+                    }
+                }
+                if (producto != 0) {
+                    numerosPrimos.add(divisor);
+                }
+                divisor = divisor + 1;
+            }
+            else{
+                numerosPrimos.add(producto);
+                divisor = divisor + 1;
+            }
+        }
+        return numerosPrimos;
+    }
+}
+
