fed942dcf860157c87e83da280adb0f01724666f
Juan Zurita <Juan.Zurita@fundacion-jala.org>
2021-05-11 23:27:38 -0400

    Test 2 was added

--- numPrimos/app/src/test/java/numPrimos/NumerosPrimosTest.java
+++ numPrimos/app/src/test/java/numPrimos/NumerosPrimosTest.java
@@ -7,10 +7,20 @@ import java.util.ArrayList;
 public class NumerosPrimosTest {
 
     @Test 
-    public void testForOne(){
+    public void numPrimosTestForOne(){
         NumerosPrimos numPrimos = new NumerosPrimos();
         ArrayList<Integer> result = numPrimos.generate(1); 
         ArrayList<Integer> expected = new ArrayList<>();
+
+        assertEquals(expected, result);
+    }
+
+    @Test 
+    public void numPrimosTestForTwo(){
+        NumerosPrimos numPrimos = new NumerosPrimos();
+        ArrayList<Integer> result = numPrimos.generate(2); 
+        ArrayList<Integer> expected = new ArrayList<>();
+        expected.add(2);
 
         assertEquals(expected, result);
     }
