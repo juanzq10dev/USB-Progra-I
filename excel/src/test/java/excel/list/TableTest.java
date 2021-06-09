70ba12c6c440a4f20983dadad1a73a16b3cddbc4
Diego Centella Lopez <Diego.Centella@fundacion-jala.org>
2021-06-08 23:01:50 -0400

    test modified

--- excel/src/test/java/excel/list/TableTest.java
+++ excel/src/test/java/excel/list/TableTest.java
@@ -8,22 +8,18 @@ import org.junit.jupiter.api.Test;
 public class TableTest {
     @Test
     public void writeShouldLetWriteATextWithAPosition() {
-        String[][] tabular = new String[26][50];
         Table table = new Table();
         String text = "50";
         Position position  = new Position(3,10);
-        String[][] positions = new String[26][50];
         boolean result = table.write(text, position);
         assertTrue(result);
     }
 
     @Test
     public void writeShouldNotLetWriteOutsideOfTheArray() {
-        String[][] tabular = new String[26][50];
         Table table = new Table();
         String text = null;
         Position position  = new Position(100,100);
-        String[][] positions = new String[26][50];
         boolean result = table.write(text, position);
         assertFalse(result);
     }
