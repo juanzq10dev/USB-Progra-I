117c03b8d4440d79ab316c99147122dbf39231ea
Diego Centella Lopez <Diego.Centella@fundacion-jala.org>
2021-06-11 08:48:10 -0400

    classes added

--- excel/src/main/java/excel/list/Table.java
+++ excel/src/main/java/excel/list/Table.java
@@ -1,24 +1,44 @@
 package excel.list;
 
+import java.util.ArrayList;
+
 public class Table {
     private String[][] tabular;
-    private Position position;
 
     public Table() {
-        String[][] tabular = new String[26][50];
-        this.tabular = tabular;
+        this.tabular = new String[50][26];
     }
 
     public boolean write(String text, Position position) {
         boolean readText = text != null;
-        boolean readPosition = position != null;
-        boolean write = readText && readPosition;
 
-        try {
-            for (String[] positions : tabular);
-        } catch (Exception e) {
-            return false;
+        int row = position.obtainRowValue();
+        int column = position.obtainColumnValue();
+        boolean readPosition = row < 50 && column < 26;
+        boolean write = readText && readPosition;
+        if(write == true) {
+            tabular[row][column] = text;
         }
         return write;
+    }
+
+    public ArrayList<String> obtainRangeList(Position initialPosition, Position finalPosition) {
+        ArrayList<String> textList = new ArrayList<String>();
+
+        int initialColumn = initialPosition.obtainColumnValue();
+        int finalColumn = finalPosition.obtainColumnValue();
+
+        int initialRow = initialPosition.obtainRowValue();
+        int finalRow = finalPosition.obtainRowValue();
+
+        for (int row = initialRow; row <= finalRow; row++) {
+            for (int column = initialColumn; column <= finalColumn; column++) {
+                String value = tabular[column][row];
+                if (value != null) {
+                    textList.add(value);
+                }
+            }
+        }
+        return textList;
     }
 }
