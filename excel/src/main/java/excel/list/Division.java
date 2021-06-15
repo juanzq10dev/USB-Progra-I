666c84a09483f7b4e8645789bafd4b921dd41f45
Valeria Valenzuela <Valeria.Valenzuela@fundacion-jala.org>
2021-06-15 09:27:11 -0400

    Replaced tabs with spaces

--- excel/src/main/java/excel/list/Division.java
+++ excel/src/main/java/excel/list/Division.java
@@ -1,25 +1,27 @@
 package excel.list;
 
-public class Division extends Operator{
+public class Division extends Operator {
 
-	public Division(Table table) {
-		super(table);
-		//TODO Auto-generated constructor stub
-	} 
+    public Division(Table table) {
+        super(table);
+        // TODO Auto-generated constructor stub
+    }
 
-	public double Calculate(Position initialPosition, Position finalPosition) {
-		double division = 0;
-		double result = 0;
-		transformList(initialPosition, finalPosition);
-		double divisor = numbers.get(0);
-		double dividend = numbers.get(numbers.size() - 1);
-		if(dividend != 0){
-		    division = divisor / dividend;
-		} else { 
-			throw new IllegalArgumentException("dividend cannot be 0");
-		}
-		result = division;  
-		double roundedResult = (double) (Math.round(result * 100)) / 100;
-		return roundedResult;
-	} 
+    @Override
+
+    public double Calculate(Position initialPosition, Position finalPosition) {
+        double division = 0;
+        double result = 0;
+        transformList(initialPosition, finalPosition);
+        double divisor = numbers.get(0);
+        double dividend = numbers.get(numbers.size() - 1);
+        if (dividend != 0) {
+            division = divisor / dividend;
+        } else {
+            throw new IllegalArgumentException("dividend cannot be 0");
+        }
+        result = division;
+        double roundedResult = (double) (Math.round(result * 100)) / 100;
+        return roundedResult;
+    }
 }
