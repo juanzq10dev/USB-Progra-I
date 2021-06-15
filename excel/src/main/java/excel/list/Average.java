276a3568926102da0586f7628b020c844872fa6d
Valeria Valenzuela <Valeria.Valenzuela@fundacion-jala.org>
2021-06-15 09:17:20 -0400

    Replaced tabs with spaces

--- excel/src/main/java/excel/list/Average.java
+++ excel/src/main/java/excel/list/Average.java
@@ -2,22 +2,22 @@ package excel.list;
 
 public class Average extends Operator {
 
-	public Average(Table table) {
-		super(table);
-		// TODO Auto-generated constructor stub
-	}
+    public Average(Table table) {
+        super(table);
+        // TODO Auto-generated constructor stub
+    }
 
-	@Override
-	public double Calculate(Position initialPosition, Position finalPosition) {
-		double division = 0;
+    @Override
+    public double Calculate(Position initialPosition, Position finalPosition) {
+        double division = 0;
 
-		transformList(initialPosition, finalPosition);
-		for (double number : numbers) {
-			result = result + number;
-		}
+        transformList(initialPosition, finalPosition);
+        for (double number : numbers) {
+            result = result + number;
+        }
 
-		division = result / numbers.size();
-		double roundedResult = (double) (Math.round(division * 100)) / 100;
-		return roundedResult;
-	}
+        division = result / numbers.size();
+        double roundedResult = (double) (Math.round(division * 100)) / 100;
+        return roundedResult;
+    }
 }
