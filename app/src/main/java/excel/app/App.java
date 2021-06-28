07afde7f3f3133ff8a4538603566d0d775259ab1
Juan Zurita <Juan.Zurita@fundacion-jala.org>
2021-06-28 01:06:55 -0400

    Interface updated

--- app/src/main/java/excel/app/App.java
+++ app/src/main/java/excel/app/App.java
@@ -1,49 +1,62 @@
 package excel.app;
 
+import excel.list.Position;
+import excel.list.Table;
 import javafx.application.Application;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
+import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-
-        TableView<String> table = new TableView<>();
-
-        TableColumn<String, String> voidColumn = new TableColumn<>("");
-        voidColumn.setReorderable(false);
-        table.getColumns().add(voidColumn);
-                    
-        for (char alphabet = 'A'; alphabet <= 'Z'; alphabet++) {
-            TableColumn<String, String> column = new TableColumn<>(alphabet + "");
-            column.setCellValueFactory(new PropertyValueFactory<>(""));
-            column.setReorderable(false);
-            table.getColumns().add(column);
-        }
-
-        for (int i = 0; i <= 50; i++) {
-            table.getItems().add("");
-        }
-
-        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-
-        TextField box = new TextField();
-        BorderPane root = new BorderPane();
-        root.setCenter(table);
-        root.setTop(box);
-
-        Scene scene = new Scene(root, Double.MAX_VALUE, 1000);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("X-cel Team project");
-        primaryStage.show();
-    }
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Table tabular = new Table();
+        TableView<Position> table = new TableView<>();
+        table.setEditable(true);
+        TableColumn<Position, Integer> voidColumn = new TableColumn<>("");
+        voidColumn.setCellValueFactory(
+                data -> new SimpleIntegerProperty(data.getValue().obtainRowValue() + 1).asObject());
+        table.getColumns().add(voidColumn);
+
+        for (char alphabet = 'A'; alphabet <= 'Z'; alphabet++) {
+            TableColumn<Position, String> column = new TableColumn<>(alphabet + "");
+            column.setCellFactory(TextFieldTableCell.forTableColumn());
+
+            column.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Position, String>>() {
+                @Override
+                public void handle(CellEditEvent<Position, String> event) {
+                    TablePosition positionTable = event.getTablePosition();
+                    int row = positionTable.getRow() + 1;
+                    int columnValue = positionTable.getColumn() + 64;
+                    char column = (char) columnValue;
+
+                    Position position = new Position(column, row);
+                    tabular.write(event.getNewValue(), position);
+                }
+            });
+            column.setMinWidth(90);
+            table.getColumns().add(column);
+            column.setReorderable(false);
+        }
+
+        for (int i = 1; i <= 50; i++) {
+            Position position = new Position('B', i);
+            table.getItems().add(position);
+        }
+
+        Scene scene = new Scene(new BorderPane(table), Double.MAX_VALUE, 1000);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
