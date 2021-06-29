442056a7eeaa4fe3b2ad3f8e8831ddabb494e694
Juan Zurita <Juan.Zurita@fundacion-jala.org>
2021-06-29 18:07:02 -0400

    Iteration changes

--- excel/src/main/java/excel/list/Sum.java
+++ excel/src/main/java/excel/list/Sum.java
@@ -4,10 +4,9 @@ public class Sum extends Operator {
 
     public Sum(Table table) {
         super(table);
-        //TODO Auto-generated constructor stub
     }
     
-    @Override  //PARA SOBREESCRIBIR EL METODO
+    @Override 
     public double Calculate(Position initialPosition, Position finalPosition) {
         transformList(initialPosition, finalPosition);
         for (double number : numbers) {
